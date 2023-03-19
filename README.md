c78f0ae6ab005cb8c231fb35eccdb017a06d2fdf
ShirinB13 <shirinbegum707@hotmail.com>
2023-03-19 00:02:17 +0000

    SQL Database and Testdata // Addpost function
    addpost function works if even if you are not in the database (it will add you to the database)

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # GB
+
 GoldBubble
+FOR DATABASE:
+source create_db.sql;
+source insert_testdata.sql;
---
+++ create_db.sql
@@ -0,0 +1,41 @@
+# Make sure the database is created before you run this script
+#     CREATE DATABASE goldbubble;
+
+# Select the database
+USE goldbubble;
+
+# Create the user which the web app will use to access the database
+DROP USER IF EXISTS 'goldbubbleapp'@'localhost';
+CREATE USER 'goldbubbleapp'@'localhost' IDENTIFIED WITH mysql_native_password BY 'qwerty';
+GRANT ALL PRIVILEGES ON goldbubble.* TO 'goldbubbleapp'@'localhost';      
+
+# Remove the tables if they already exist
+DROP TABLE IF EXISTS posts;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS topics;
+
+# Create the user table to store user details
+CREATE TABLE users (
+  firstname VARCHAR(20) NOT NULL,
+  surname VARCHAR(20) NOT NULL,
+  username VARCHAR(15) NOT NULL UNIQUE,
+  email VARCHAR(100),
+  PRIMARY KEY(username)
+);
+
+# Create the topics table to store the list of available topics
+CREATE TABLE topics (
+   topic_title VARCHAR(50),
+   PRIMARY KEY(topic_title)
+);
+
+# Create the posts table to store the user posts
+CREATE TABLE posts (
+    post_date DATETIME,
+    post_title VARCHAR(100),
+    post_content VARCHAR(2000),
+    username VARCHAR(15),
+    topic_title VARCHAR(50),
+    FOREIGN KEY (username) REFERENCES users(username),
+    FOREIGN KEY (topic_title) REFERENCES topics(topic_title)
+);
